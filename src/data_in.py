0d73e73ce60a71188836dcbc343aa58c673ba79f
Ethan Malin <ethanmalin01@gmail.com>
2019-02-19 21:59:03 -0600

    Test (#13)
    * skeleton of tests
    
    * Testing storification
    
    * Still testing
    
    * changing database name to match update
    
    * Mostly working test case
    
    * Add ability to mine main image byline as well as image. (#11)
    
    * Add tweet detection in article body. (#12)

--- src/data_in.py
+++ src/data_in.py
@@ -1,17 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-SEARCH_PAGE_URL = 'http://www.kansan.com/search/?s=start_time&sd=asc&l=25&t=article&nsa=eedition'
-TEST_ARTICLE_OLD = 'http://www.kansan.com/news/ice-storm-remnants-pose-threat/article_6dc5f5b6-0ca9-59e9-8572-11f167ae9443.html'
-TEST_ARTICLE_MIDDLE = 'http://www.kansan.com/news/that-s-disgusting-bugs-in-food/article_25562341-d7e2-554d-aa47-94653a5e3038.html'
-TEST_ARTICLE_NEW = 'http://www.kansan.com/sports/ku-track-and-field-splits-up-for-record-breaking-first/article_563c7476-2c4d-11e9-b5c8-d3a4a0cc9ae9.html'
-
 class Story:
 
     #constructor
-    def __init__(self, url, main_image, headline, author, date, body):
+    # NOTE: MISSING 'SECTION' as in arts-and-culture or sports or whatever
+    def __init__(self, url, main_image, mib, headline, author, date, body):
         self.url = url
         self.main_image = main_image
+        self.main_image_byline = mib
         self.headline = headline
         self.author = author
         self.date = date
@@ -36,10 +33,13 @@ class Story:
 def get_story(article_url):
     return(_article_to_story(article_url))
 
+def list_stories(search_url):
+    return _get_urls(search_url)
+
 # params: <String> url of search page
-# return: <List<Story>> results of rss search
+# return: <List<Story>> results of search
 def get_stories(search_url):
-    search_stories = list()
+    search_stories = []
     for art in _get_urls(search_url):
         search_stories.append(_article_to_story(art))
     return search_stories
@@ -63,9 +63,9 @@ def _get_urls(search_url):
         article_urls.append(story_url)
 
     # get next results url ??
-    button_soup = soup.find('div', class_='pagination-container')
-    next_results_url = _get_next_results_url(button_soup)
-    print(next_results_url)
+    #button_soup = soup.find('div', class_='pagination-container')
+    #next_results_url = _get_next_results_url(button_soup)
+    #print(next_results_url)
     return article_urls
 
 # params: <String> url of article
@@ -75,6 +75,8 @@ def _article_to_story(article_url):
     source = requests.get(article_url).text
     soup = BeautifulSoup(source, 'lxml')
     article = soup.find('article')
+    
+    print("mining story ", article_url)
 
     # required
     headline = _mine_headline(article)
@@ -82,20 +84,25 @@ def _article_to_story(article_url):
     date = _mine_date(article)
 
     # optional
-    main_image = _mine_main_image(article)
+    main_image, img_byline = _mine_main_image(article)
     body = _mine_body(article)
 
-    s = Story(article_url, main_image, headline, author, date, body)
+    s = Story(article_url, main_image, img_byline, headline, author, date, body)
     print('STORY:\n', s, 'END STORY\n\n')
+    print('Main img URL: ', main_image)
+    print('Main img byline: ' + img_byline)
+    print('BODY: ', body)
 
     return(s)
 
 def _mine_main_image(article):
     img = article.find('div', id='asset-photo')
+    img_byline = img.find('figcaption', class_='caption').find('p').text
+
     if(img is None):
         return None
     else:
-        return img.find('meta', itemprop='contentUrl')['content']
+        return img.find('meta', itemprop='contentUrl')['content'], img_byline
      
 
 def _mine_headline(article):
@@ -129,7 +136,11 @@ def _mine_body(article):
             # TODO: Get the byline of the image, but how to store?? as a double?
             imgsrc = '$$$IMAGE$$$' + child.find('meta', itemprop='contentUrl')['content']
             body.append(imgsrc)
-
+        elif child.name == 'blockquote':
+            print('\n\n***DEBUG*** Tweet located.\n')
+            tweet_url = '$$$TWEET$$$' + child.find_all('a')[-1]['href']
+            body.append(tweet_url)
+            
         else:
             print("UNMATCHED ELEMENT IN ARTICLE BODY", child)
 
@@ -168,6 +179,3 @@ def _get_next_results_url(results_url):
         results_url += num# add the new one
         return(results_url)
 
-test = get_story(TEST_ARTICLE_OLD)
-test = get_story(TEST_ARTICLE_MIDDLE)
-test = get_story(TEST_ARTICLE_NEW)
--- src/store.py
+++ src/store.py
@@ -2,27 +2,14 @@ import data_in
 from datetime import date, datetime
 import mysql.connector
 
-# connect to database (local for testing)
-db = mysql.connector.connect(host="localhost", user="root", password="daily_kansan_news", database="UDK_APP_TEST")
-cursor = db.cursor()
-
-# print all in database
-cursor.execute("SELECT * FROM Fake_News")
-result = cursor.fetchall()
-for x in result:
-    print(x)
-
-# print again
-cursor.execute("SELECT * FROM Fake_News")
-result = cursor.fetchall()
-for x in result:
-    print(x)
-
-# need error handling here
+# need error handling!!
 def put_story(s):
-    db = mysql.connector.connect(host="104.248.235.9", user="root", password="daily_kansan_news", database="NEWS_V1")
+    db = mysql.connector.connect(host="localhost", user="remote", password="sugarsugar", database="UDK")
     cursor = db.cursor()
-    q = '''INSERT INTO News_V1 (url, headline, author, date, main_image, body) VALUES (%s, %s, %s, %s, %s, %s)'''
-    data = (s.url, s.headline, s.author, s.date, s.main_image, s.body)
-    cursor.execute(q, data)
+    q = '''INSERT INTO Articles (url, headline, author, date, main_image, body) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')''' % (s.url, s.headline, s.author, s.date, s.main_image, s.body)
+    print(q)
+
+    cursor.execute(q)
+    db.commit()
+    print("Stored successfully!")
 
