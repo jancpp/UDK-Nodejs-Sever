386ebba2c4e69626bde037615244bdb09987ce34
Ethan Malin <ethanmalin01@gmail.com>
2019-03-07 19:17:58 -0600

    using prepared statements in sql query

--- src/store.py
+++ src/store.py
@@ -7,11 +7,12 @@ def put_story(s):
 
     try:
         db = mysql.connector.connect(host="localhost", user="remote", password="sugarsugar", database="UDK")
-        cursor = db.cursor()
-        q = "INSERT INTO ARTICLES (url, headline, author, date, main_image, main_image_byline, body, category) VALUES ({}, {}, {}, {}, {}, {}, {}, {});".format(s.url, s.headline, s.author, s.date, s.main_image, s.main_image_byline, s.body, s.category)
+        cursor = db.cursor(prepared=True)
+        q = """INSERT INTO ARTICLES (url, headline, author, date, main_image, main_image_byline, body, category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
+        input = (s.url, s.headline, s.author, s.date, s.main_image, s.main_image_byline, s.body, s.category)
         print(q)
 
-        cursor.execute(q)
+        cursor.execute(q, input)
         db.commit()
 
         print("Stored successfully!")
