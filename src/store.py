54b146797411b74c7e2ee11336d5a329df74dbab
Ethan Malin <ethanmalin01@gmail.com>
2019-03-07 19:30:13 -0600

    reverting change

--- src/store.py
+++ src/store.py
@@ -7,7 +7,7 @@ def put_story(s):
 
     try:
         db = mysql.connector.connect(host="localhost", user="remote", password="sugarsugar", database="UDK")
-        cursor = db.MySQLCursorPrepared()
+        cursor = db.cursor(prepared=True)
         q = """INSERT INTO ARTICLES (url, headline, author, date, main_image, main_image_byline, body, category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
         input = (s.url, s.headline, s.author, s.date, s.main_image, s.main_image_byline, s.body, s.category)
         print(q)
